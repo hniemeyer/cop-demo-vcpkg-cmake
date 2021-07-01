ef749303a13f30b855afed7323f9f3c250b5cad0
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-07-01 10:21:48 +0200

    renamed tests

--- tests/first_test.cpp
+++ tests/first_test.cpp
@@ -1,10 +1,10 @@
 #include "gtest/gtest.h"
 #include "math.hpp"
 
-TEST(Global, Test1) {
-    EXPECT_EQ(1, 1);
+TEST(Mathtest, Test1) {
+    EXPECT_EQ(0, add(-1,1));
 }
 
-TEST(Global, Test2) {
+TEST(Mathtest, Test2) {
     EXPECT_EQ(8, add(3,5));
 }
