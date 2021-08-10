92cdbea87bfab8d65e3fe9dce299ce68f16786c9
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-08-10 14:17:25 +0200

    find external libs in global CMakeLists.txt

--- cmake/GenerateProjects.cmake
+++ cmake/GenerateProjects.cmake
@@ -10,12 +10,10 @@ function(generate_application)
 						 "${multiValues}"
 						  ${ARGN})
 
-	foreach(arg IN LISTS ${ARG_EXT_DEPENDENCIES})
-	find_package(${arg} CONFIG REQUIRED)
-	endforeach()
+
+	message("ext deps: ${ARG_EXT_DEPENDENCIES}")
 
 
-	find_package(fmt CONFIG REQUIRED)
 	file(GLOB_RECURSE SRC_FILES src/*.cpp)
 	add_executable (${ARG_PROJ_NAME} ${SRC_FILES})
 	target_link_libraries(${ARG_PROJ_NAME} PRIVATE project_warnings ${ARG_EXT_DEPENDENCIES} ${ARG_INT_DEPENDENCIES})
